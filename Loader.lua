66e4a81770af6178348b4416b621e64ad5a9b672
ObiTobie <hadesjus@gmail.com>
2025-11-11 11:18:23 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
 local placeId = game.GameId
 if placeId == 648454481 then --gpo
-	loadstring(game:HttpGet(""))()
+	loadstring(game:HttpGet("https://github.com/ObiTobie/Luxury/blob/main/src/Luxury.lua?raw=true"))()
 end
