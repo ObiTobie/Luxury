5d0c30f68f2c84971083687d5566a4f377e0a94d
ObiTobie <hadesjus@gmail.com>
2025-11-23 22:51:34 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,19 @@
-local placeId = game.GameId
-if placeId == 648454481 then --gpo
-	loadstring(game:HttpGet("https://github.com/ObiTobie/Luxury/blob/main/src/Luxury.lua?raw=true"))()
+local Services							= setmetatable({}, { __index = function(Self, Key) return game.GetService(game, Key) end })
+local Client							= Services.Players.LocalPlayer
+local GameID							= game.GameId
+
+if not getgenv().Configs then
+	getgenv().Configs = {
+	    Mode = "Normal",
+	}
+end
+
+if GameID == 648454481 then --// Grand Piece Online
+	if getgenv().Configs.Mode == "Kaitan" then
+		loadstring(game:HttpGet("https://github.com/ObiTobie/Luxury/blob/main/src/Kaitan.lua?raw=true"))()
+	else
+		loadstring(game:HttpGet("https://github.com/ObiTobie/Luxury/blob/main/src/Luxury.lua?raw=true"))()
+	end
+else
+	Client:Kick("ลูกหรี่ชอบโง่รันผิดแมพ")
 end
